57c09bedfaa7bec828801c370f91bcf6cd3b107a
laytsyn <ndtrubitsyn@edu.tversu.ru>
2023-09-30 09:04:55 +0300

    Инициализация проекта

---
+++ src/test/java/ru/cloud/storage/backendjavacloudstorage/BackendJavaCloudStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.cloud.storage.backendjavacloudstorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendJavaCloudStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
