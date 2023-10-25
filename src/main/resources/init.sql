ab6fd84f072b837a4caabf6910d2f019866bbb70
laytsyn <ndtrubitsyn@edu.tversu.ru>
2023-10-25 21:14:31 +0300

    Добавление небольшой валидации адреса электронной почты

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -2,6 +2,6 @@ create table users(
     id  BIGSERIAL PRIMARY KEY,
     firstname character varying(128) NOT NULL,
     lastname character varying(128),
-    email character varying(128) NOT NULL UNIQUE,
+    email character varying(128) NOT NULL UNIQUE CHECK(email ~* '^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'),
     hashpassword character varying(128) NOT NULL
 );
