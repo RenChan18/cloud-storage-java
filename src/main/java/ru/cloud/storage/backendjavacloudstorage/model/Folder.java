2f24a6d5e45d7a770595d19af541a4a2f65ce3c5
GreenCalf <maxlosev@28@gmail.com>
2023-10-25 22:03:27 +0300

    Примеры работы

--- src/main/java/ru/cloud/storage/backendjavacloudstorage/model/Folder.java
+++ src/main/java/ru/cloud/storage/backendjavacloudstorage/model/Folder.java
@@ -1,7 +1,10 @@
 package ru.cloud.storage.backendjavacloudstorage.model;
 
 
-import jakarta.persistence.*;
+import jakarta.persistence.Basic;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
