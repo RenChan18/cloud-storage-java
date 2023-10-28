6bb34384f9a25e7e2f1d7f3191d16d789016800d
laytsyn <ndtrubitsyn@edu.tversu.ru>
2023-10-28 18:17:36 +0300

    Добавление триггера для PostgreSQL, переводящего данные почты в нижний регистр при добавлении или обновлении записи

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -5,3 +5,14 @@ create table users(
     email character varying(128) NOT NULL UNIQUE CHECK(email ~* '^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'),
     hashpassword character varying(128) NOT NULL
 );
+
+CREATE OR REPLACE FUNCTION tolowercase_email() 
+RETURNS trigger AS $tolowercase_email$
+    BEGIN        
+        NEW.email = LOWER(NEW.email);
+        RETURN NEW;
+    END;
+$tolowercase_email$ LANGUAGE plpgsql;
+
+CREATE TRIGGER tolowercase_email BEFORE INSERT OR UPDATE ON users
+    FOR EACH ROW EXECUTE PROCEDURE tolowercase_email();
